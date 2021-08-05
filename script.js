a7c4a0996a9a24f4c37146359d72b9f0ee24b4f9
fel1x0u <73656401+fel1x0u@users.noreply.github.com>
2021-08-05 10:19:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function loadScript() {
 	location.href = "/files/mc.mp4";
 }
-function loadBootstrapCss() {
+function changeLocation24() {
 	location.href = "/files/music/pancake robot.mp4";
 }
 function smth() {
 	location.href = "/files/music/undone.mp3";
+}
+
+function changeLocation(newl) {
+	location.href = new1;
 }
